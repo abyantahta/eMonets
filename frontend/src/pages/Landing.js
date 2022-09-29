7b6c2f1f3a8492b3fa6615fdcc254b30dd17d8a1
Abyantahta <abyantahta2002@gmail.com>
2022-09-29 13:50:15 +0700

    register page

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/style.scss"
+function Landing() {
+  return (
+    <div className="Landing">
+        <h1>halo ini landing</h1>
+    </div>
+  )
+}
+
+export default Landing
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import "../styles/style.scss"
+import registerHero from '../images/registerHero.png'
+import logo from '../images/logo.png'
+import {BsPerson} from 'react-icons/bs'
+import {VscMail} from 'react-icons/vsc'
+import {BsKey} from 'react-icons/bs'
+import {BsEye} from 'react-icons/bs'
+import {BsEyeSlash} from 'react-icons/bs'
+import {useState} from 'react'
+
+function Register() {
+    const [passwordType, setPasswordType] = useState("password");
+    const [passwordInput, setPasswordInput] = useState("");
+    const handlePasswordChange =(e)=>{
+        setPasswordInput(e.target.value);
+        console.log(e.target.value)
+    }
+    const togglePassword =()=>{
+      if(passwordType==="password")
+      {
+       setPasswordType("text")
+       return;
+      }
+      setPasswordType("password")
+    }
+
+      const [confirmPasswordType, setConfirmPasswordType] = useState("password");
+    const [confirmPasswordInput, setConfirmPasswordInput] = useState("");
+    const handleConfirmPasswordChange =(e)=>{
+        setConfirmPasswordInput(e.target.value);
+        console.log(e.target.value)
+    }
+    const toggleConfirmPassword =()=>{
+      if(confirmPasswordType==="password")
+      {
+       setConfirmPasswordType("text")
+       return;
+      }
+      setConfirmPasswordType("password")
+    }
+
+  return (
+    <div className="form">
+      <div className="left">
+        <img src={registerHero} alt="" />
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+      </div> 
+      <div className="right">
+        <div className="contentContainer">
+          <form action="">
+          <h1 className="formTitle">Register</h1>
+            <div className="input">
+              <i><BsPerson/></i>
+              <input type="text" name='username' placeholder='username'/>
+            </div>
+            <div className="input">
+              <i><VscMail/></i>
+              <input type="email" name='email' placeholder='email'/>
+            </div>
+            <div className="input">
+              <i><BsKey/></i>
+              <input type={passwordType} onChange={handlePasswordChange} name='password' placeholder='password' value={passwordInput}/>
+              <div className="showPassword" onClick={togglePassword}>
+                {passwordType==="password"? <BsEye/> : <BsEyeSlash/> }
+              </div>
+            </div>
+            <div className="input">
+              <i><BsKey/></i>
+              <input type={confirmPasswordType} onChange={handleConfirmPasswordChange} name='password' placeholder='confirm password' value={confirmPasswordInput}/>
+              <div className="showPassword" onClick={toggleConfirmPassword}>
+                {confirmPasswordType==="password"? <BsEye/> : <BsEyeSlash/> }
+              </div>
+            </div>
+            <button type="submit">Register</button>
+          </form>
+          <h3>Already have an account? <span><a href="">Login</a></span></h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
